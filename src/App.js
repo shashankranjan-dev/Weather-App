74c3ec36cd128af1aa6390988d8c509db1fe785f
Shashankranjan-dev <shashankranjan970832@gmail.com>
2021-12-22 18:52:36 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HomePage from './HomePage/HomePage';
+import Search from './search/search';
+
+function App() {
+  return (
+    <div className="main">
+        <HomePage />
+        <Search />
+    </div>   
+
+  );
+}
+
+export default App;
